257b783ece2d51955f712e7825ad210bc033cec5
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-20 23:40:03 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExample
+{
+    public class Car
+    {
+        public Car()//created constructor method; a special member method that has same name as Class
+        {
+        }
+        //fields
+        //properties
+        //methods
+
+        public string Make { get; set; }
+        public string Model { get; set; }   
+        public int Year { get; set; }
+
+        
+    }  //end of class  
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassesExample;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        {
+            myCar.Make = "Toyota";
+            myCar.Model = "Highlander";
+            myCar.Year = 2013;
+        }
+
+        Car mazda = new Car();
+        {
+            mazda.Make = "Mazda";
+            mazda.Model = "CX-5";
+            mazda.Year = 2013;
+        }
+
+        Car jeep = new Car();
+        {
+            jeep.Make = "Jeep";
+            jeep.Model = "Cherokee";
+            jeep.Year = 2008;
+        }
+
+
+        var carList = new List<Car>() {myCar, mazda, jeep };
+        foreach (var car in carList) 
+        {
+            Console.WriteLine($"{car.Make}, {car.Model}, {car.Year}");
+        }
+    }
+}
